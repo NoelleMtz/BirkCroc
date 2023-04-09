e0989ebd7ab03a770e98f7c928d814e0ea25495a
NoelleMtz <noowmart@gmail.com>
2023-04-09 12:41:47 -0600

    Sunday B/N

---
+++ api_keys.py
@@ -0,0 +1,4 @@
+# Noelle's keys
+weather_api_key = "4fdf3c34f5c29348468fa0c7f1fbb20c"
+census_key = "894274dad3d33879c1be28921afcb9b5568e9af4"
+google_api = "26dfbcddf78ff3c6f8391e9b47b6fb3ecaef64101ecbeacc509d2148b09c3eac"
